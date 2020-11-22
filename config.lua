993cdcb9fd7c9d8c615de2ff57e82b6bf8601142
Praryo <cyrrilericv@gmail.com>
2020-11-22 17:28:34 +0800

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,50 @@
+Config = {}
+
+Config.InitialRentPrice = 450
+Config.DailyRentPrice = 250
+
+Config.LockerBlips = {
+    ["Locker"] = {
+        name = "Locker Room",
+        mapBlip = {x = -286.23, y = 280.84, z = 89.89, color = 3, sprite = 357, size = 1.0}
+    },
+}
+
+Config.LockerExterior = vector3(-286.23, 280.84, 89.89)
+
+Config.LockerInterior = vector3(1173.24, -3196.62, -39.01)
+
+
+Config.Lockers = {
+
+	["locker1"] = {
+		locker_name = 'Locker #1',
+		location = vector3(1161.87, -3199.07, -39.01),
+	},
+	
+	["locker2"] = {
+		locker_name = 'Locker #2',
+		location = vector3(1156.71, -3195.3, -39.01),
+	},
+	
+	["locker3"] = {
+		locker_name = 'Locker #3',
+		location = vector3(1157.61, 3198.92, -39.01),
+	},
+	
+	["locker4"] = {
+		locker_name = 'Locker #4',
+		location = vector3(1167.05, -3194.64, -39.01),
+	},
+	
+	["locker5"] = {
+		locker_name = 'Locker #5',
+		location = vector3(1173.29, -3194.47, -39.01),
+	},
+	
+	["locker6"] = {
+		locker_name = 'Locker #6',
+		location = vector3(1171.73, -3198.81, -39.01),
+	},
+	
+}
